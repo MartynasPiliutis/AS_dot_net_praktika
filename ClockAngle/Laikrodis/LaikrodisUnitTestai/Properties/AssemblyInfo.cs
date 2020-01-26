8a1e45fba77ec9abcb372538aac65b97d10f08a6
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-26 17:14:18 +0200

    LaikrodisFinal sukurtos klases

---
+++ ClockAngle/Laikrodis/LaikrodisUnitTestai/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LaikrodisUnitTestai")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaikrodisUnitTestai")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0678193-6824-41cd-a6f0-df21420cc9ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
