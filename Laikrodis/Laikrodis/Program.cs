e8f8f426e2969725dcdc51edc9b20a51f0f4377a
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-26 02:43:22 +0200

    Pilnai veikiantis kodas. Be testu ir konstruktoriu

--- Laikrodis/Laikrodis/Program.cs
+++ Laikrodis/Laikrodis/Program.cs
@@ -10,23 +10,61 @@ namespace Laikrodis
     {
         static void Main(string[] args)
         {
-            decimal minutes = 2m;
-            decimal valandos = 2m;
+            int minutes;
+            int valandos;
             decimal mazojiRodykle;
             decimal didziojiRodykle;
             decimal kampas;
             Console.Write("Iveskite valandas: ");
-            valandos = Convert.ToDecimal(Console.ReadLine());
+            bool successValandos = Int32.TryParse(Console.ReadLine(), out valandos);
+            if (!successValandos)
+            {
+                Console.WriteLine("Neteisingai pateiktos valandos...");
+                Console.WriteLine("Spauskite bet kuri kad baigti...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else if (valandos > 23 || valandos < 0)
+            {
+                Console.WriteLine($"Neteisingai pateiktos valandos: {valandos}");
+                Console.WriteLine("Spauskite bet kuri kad baigti...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
             Console.Write("Iveskite minutes: ");
-            minutes = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine($"Ivestas laikas yra {valandos, 2}:{minutes, 2}");
+            bool successMinutes = Int32.TryParse(Console.ReadLine(), out minutes);
+            if (!successMinutes)
+            {
+                Console.WriteLine("Neteisingai pateiktos valandos: {0}", minutes);
+                Console.Write("Spauskite bet kuri kad baigti...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else if (minutes > 59 || minutes < 0)
+            {
+                Console.WriteLine($"Neteisingai pateiktos minutes: {minutes}");
+                Console.WriteLine("Spauskite bet kuri kad baigti...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"Ivestas laikas yra {valandos.ToString("D2")}:{minutes.ToString("D2")}");
             Console.ReadLine();
+            if (valandos > 12)
+            {
+                valandos -= 12;
+            }
             didziojiRodykle = 6 * minutes;
             mazojiRodykle = 30 * valandos + 0.50m * minutes;
             kampas = mazojiRodykle - didziojiRodykle;
-            Console.WriteLine($"Dizioji rodykle {didziojiRodykle} laipsniai");
-            Console.WriteLine($"Mazoji rodykle {mazojiRodykle} laipsniai");
-            Console.WriteLine($"Kampas tarp rodykliu {kampas}");
+            if (kampas > 180)
+            {
+                kampas -= 360;
+            }
+            if (kampas < 0)
+            {
+                kampas *= -1;
+            }
+            Console.WriteLine($"Kampas tarp rodykliu yra {kampas}");
             Console.ReadLine();
         }
     }
